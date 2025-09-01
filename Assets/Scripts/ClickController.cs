399a24c25b5b19054abac1193923009232c2628d
Leejinyoung126 <shinyshines76@gmail.com>
2025-09-01 20:32:24 +0900

    Feat

---
+++ Assets/Scripts/ClickController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickController : MonoBehaviour
+{
+    public bool isPaused = false;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPaused)
+        {
+            return;
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            if(EventSystem.current != null && EventSystem.current.IsPointerOverGameObject())
+            {
+                return;
+            }
+            Debug.Log("Attack!");
+        }
+    }
+}
---
+++ Assets/Scripts/Dummy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy : MonoBehaviour
+{
+    private Animator anim;
+
+    private const string DummyAnim = "Dummy";
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(int  damage)
+    {
+        anim.Play(DummyAnim, 0, 0f);
+        Debug.Log("Dummy hit");
+    }
+}
