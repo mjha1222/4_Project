de6ce9d0b562a61ea6156c1d89b661b3d894e179
lee-hyeonsu-1 <dbsdnrl123@gmail.com>
2025-09-03 12:35:20 +0900

    feat 적 데이터 저장

--- Assets/Scripts/HP bar/Enemy.cs
+++ Assets/Scripts/HP bar/Enemy.cs
@@ -2,14 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enum : MonoBehaviour
+public class Enemy : MonoBehaviour
 {
-    public enum EnemyType 
-    {
-        Circle,
-        Triangle,
-        Square,
-    }
+    public EnemyData Data;
 
     // Start is called before the first frame update
     void Start()
